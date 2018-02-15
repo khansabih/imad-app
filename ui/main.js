e99c058f60708762afc997fd5b76e08d8d24c0cf
khansabih <36441298+khansabih@users.noreply.github.com>
2018-02-15 22:06:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,11 @@
 console.log('Loaded!');
 var element = getElementById('mainText');
 element.innerHTML = "New Value";
+
+// move the image
+
+var imageElement = getElementById('buddy');
+imageElement.onClick = function(){
+  imageElement.style.marginLeft='100px';  
+};
+
